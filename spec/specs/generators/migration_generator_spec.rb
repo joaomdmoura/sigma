c6659b42599f8dc3e7ef0c1662f6703625f82347
Joao Moura <joaomdmoura@gmail.com>
2013-05-11 01:30:34 -0300

    updating tests

--- spec/specs/generators/migration_generator_spec.rb
+++ spec/specs/generators/migration_generator_spec.rb
@@ -12,5 +12,17 @@ describe "Migration Generator" do
     it "Should have the Doubt field with default value" do
       user.doubt.should == scale/6
     end
+
+    it "Should have 0 wins" do
+      user.wins.should == 0
+    end
+
+    it "Should have 0 losses" do
+      user.losses.should == 0
+    end
+
+    it "Should have 0 draws" do
+      user.draws.should == 0
+    end
   end
 end
