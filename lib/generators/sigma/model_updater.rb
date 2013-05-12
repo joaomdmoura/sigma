2b23ea92e1c59f7876673c39a675ea1eb93cf5f6
Joao Moura <joaomdmoura@gmail.com>
2013-05-12 04:32:49 -0300

    fix inject into file call inside model updater and updating resource model template

--- lib/generators/sigma/model_updater.rb
+++ lib/generators/sigma/model_updater.rb
@@ -2,9 +2,10 @@ module ModelUpdater
   def add_methods
     resource = File.read find_in_source_paths("resource_model.rb")
     inject_into_file  "app/models/#{@model_name}.rb",
-                          "\n#{resource}\n",
-                          :after => ":float"
-
-    inject_into_class "app/models/#{@model_name}.rb", @model_name.capitalize, "\n#{resource}\n"
+                      ":skill, :doubt, ",
+                      :after => "attr_accessible "
+		inject_into_file  "app/models/#{@model_name}.rb",
+                      "\n#{resource}\n",
+                      :after => /attr_accessible [a-z:, ]+$/
   end
 end
--- lib/generators/templates/resource_model.rb
+++ lib/generators/templates/resource_model.rb
@@ -1,7 +1,7 @@
-scope :ranking, select("<%= @model_name.pluralize %>.*")
-    .select("(<%= @model_name.pluralize %>.skill - 3 * <%= @model_name.pluralize %>.doubt) as rating")
+	scope :ranking, select("#{self.name.pluralize}.*")
+    .select("(#{self.name.pluralize}.skill - 3 * #{self.name.pluralize}.doubt) as rating")
     .order("rating DESC")
 
-def rating
-  @rating ||= self[:rating] || self.skill - 3 * self.doubt
-end
+	def rating
+	  @rating ||= self[:rating] || self.skill - 3 * self.doubt
+	end
