3f0d54fe5e0519cc7fe3324680394e2c2ae91220
Joao Moura <joaomdmoura@gmail.com>
2013-05-13 02:51:27 -0300

    adding database cleaner and removing coveralls

--- Gemfile
+++ Gemfile
@@ -6,5 +6,5 @@ gem 'rspec-rails',        '2.13.0'
 gem 'rspec',              '2.13.0'
 gem 'rails',              '3.2.13'
 gem 'factory_girl_rails', '4.2.1'
-gem 'coveralls', require: false
 gem 'sqlite3'
+gem 'database_cleaner'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,23 @@
 # Configure Rails Envinronmnt
-# require 'coveralls'
-# Coveralls.wear!
+require 'database_cleaner'
 
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 Rails.backtrace_cleaner.remove_silencers!
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
