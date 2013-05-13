ffb1817fbf190554b169aa586a676263a29619ec
Joao Moura <joaomdmoura@gmail.com>
2013-05-13 15:14:00 -0300

    adding matches method and starting won method implementation

--- lib/generators/templates/resource_model.rb
+++ lib/generators/templates/resource_model.rb
@@ -1,3 +1,5 @@
+	serialize :expectations
+
   scope :ranking, select("#{self.name.pluralize}.*")
     .select("(#{self.name.pluralize}.skill - 3 * #{self.name.pluralize}.doubt) as rating")
     .order("rating DESC")
@@ -8,4 +10,18 @@
 
   def position
     self.class.ranking.index(self)+1
+  end
+
+  def matches
+    self.wins+self.losses+self.draws
+  end
+
+	def won(resource)
+    x = rating - resource.rating
+    y = x > 0
+    z = resource_probability(y)
+  end
+
+  def resource_probability(y)
+    #TODO
   end
