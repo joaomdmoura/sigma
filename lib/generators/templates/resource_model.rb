296ceaf3409869c03c00c08ae36aba068ad407c5
Joao Moura <joaomdmoura@gmail.com>
2013-05-13 02:15:38 -0300

    updating resource model template

--- lib/generators/templates/resource_model.rb
+++ lib/generators/templates/resource_model.rb
@@ -1,8 +1,11 @@
-  def self.ranking
-    ActiveRecord::Base.connection.execute("SET @i = 0;")
-    ActiveRecord::Base.connection.select_all("SELECT #{self.name.pluralize}.*, (#{self.name.pluralize}.skill - 3 * #{self.name.pluralize}.doubt) as rating, (@i := @i+1) as position FROM #{self.name.pluralize} ORDER BY rating DESC;")
-  end
+  scope :ranking, select("#{self.name.pluralize}.*")
+    .select("(#{self.name.pluralize}.skill - 3 * #{self.name.pluralize}.doubt) as rating")
+    .order("rating DESC")
 
   def rating
-    @rating ||= self[:rating] || self.skill - 3 * self.doubt
+    self.skill - 3 * self.doubt
+  end
+
+  def position
+    self.class.ranking.index(self)+1
   end
