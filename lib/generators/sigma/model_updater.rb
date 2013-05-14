7011a2c48b02ad98cb5a8131834978b39639bfa3
Joao Moura <joaomdmoura@gmail.com>
2013-05-14 15:55:59 -0300

    adding wins, losses and draws as accessible

--- lib/generators/sigma/model_updater.rb
+++ lib/generators/sigma/model_updater.rb
@@ -2,7 +2,7 @@ module ModelUpdater
   def add_methods
     resource = File.read find_in_source_paths("resource_model.rb")
     inject_into_file  "app/models/#{@model_name}.rb",
-                      ":skill, :doubt, :expectations, ",
+                      ":skill, :doubt, :wins, :losses, :draws, :expectations, ",
                       :after => "attr_accessible "
     inject_into_file  "app/models/#{@model_name}.rb",
                       "\n#{resource}\n",
