c479f2494b20acbd22a71529566bef53e077d3b1
Joao Moura <joaomdmoura@gmail.com>
2013-05-14 15:56:21 -0300

    add resource probability method and starting won method

--- lib/generators/templates/resource_model.rb
+++ lib/generators/templates/resource_model.rb
@@ -16,12 +16,30 @@
     self.wins+self.losses+self.draws
   end
 
-	def won(resource)
-    x = rating - resource.rating
-    y = x > 0
-    z = resource_probability(y)
+	def won(probability)
+    expectation      = (probability == 0) ? 0 : probability > 0
+    real_expectation = resource_probability(expectation)
   end
 
-  def resource_probability(y)
-    #TODO
+  def resource_probability(expectation)
+    result = (expectation == true) ? 'wins' : nil
+    result ||= (expectation == false) ? 'losses' : 'draws'
+
+    w = self.wins*100.0/matches
+    l = self.losses*100.0/matches
+    d = self.draws*100.0/matches
+
+    expectations = {
+                      'wins'   =>  { we:0, le:0, de:0 },
+                      'losses' =>  { we:0, le:0, de:0 },
+                      'draws'  =>  { we:0, le:0, de:0 }
+                   }
+
+    expectations.each do |k, v|
+      v[:we] = w*(self.expectations['win_expectation'][k]*100.0/self[k])
+      v[:le] = l*(self.expectations['lost_expectation'][k]*100.0/self[k])
+      v[:de] = d*(self.expectations['draw_expectation'][k]*100.0/self[k])
+    end
+
+    (expectations[result][:we] / (expectations[result][:we] + expectations[result][:le] + expectations[result][:de]))*100
   end
