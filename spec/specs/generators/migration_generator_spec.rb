1ba87af1e23dfbc42f1308717f0d3a1443608664
Joao Moura <joaomdmoura@gmail.com>
2013-06-30 05:22:18 -0300

    removing should of tests

--- spec/specs/generators/migration_generator_spec.rb
+++ spec/specs/generators/migration_generator_spec.rb
@@ -5,27 +5,27 @@ describe "migration generator" do
   let(:scale) { 50.0 }
   
   context "check if the migrations updated user table" do
-    it "should have the skill field and the default value" do
+    it "have the skill field and the default value" do
       user.skill.should == scale/2
     end
 
-    it "should have the doubt field and the default value" do
+    it "have the doubt field and the default value" do
       expect(user.doubt).to eq scale/6
     end
 
-    it "should have 0 wins" do
+    it "have 0 wins" do
       expect(user.wins).to eq 0
     end
 
-    it "should have 0 losses" do
+    it "have 0 losses" do
       expect(user.losses).to eq 0
     end
 
-    it "should have 0 draws" do
+    it "have 0 draws" do
       expect(user.draws).to eq 0
     end
 
-    it "should have a serialized expectations x result field" do
+    it "have a serialized expectations x result field" do
       expect(user.expectations).to_not be_nil
       expect(user.expectations['win_expectation']).to_not be_nil
       expect(user.expectations['lost_expectation']).to_not be_nil
--- spec/specs/generators/model_updater_spec.rb
+++ spec/specs/generators/model_updater_spec.rb
@@ -6,7 +6,7 @@ describe "model updater" do
   let(:user_3) { create :user, :skill => 50, :doubt => 4 }
   
   context "check if ranking scope was correctly added" do
-    it "should return the ranking with the correct rating order" do
+    it "return the ranking with the correct rating order" do
       expect(User.ranking).to eq [user_3, user_2, user_1]
     end    
   end
@@ -14,21 +14,21 @@ describe "model updater" do
   context "check if the template method were added to resource model" do
 
     describe ".ranking" do
-      it "should return 0 as de default rating for a user_1" do
+      it "return 0 as de default rating for a user_1" do
         expect(user_1.rating).to eq 0
       end
       
-      it "should return the result of rating algorithm" do
+      it "return the result of rating algorithm" do
         expect(user_2.rating).to eq user_2.skill-3*user_2.doubt
       end
     end
 
     describe ".position" do
-      it "should return the position of a specific resource in ranking" do
+      it "return the position of a specific resource in ranking" do
         expect(user_3.position).to eq User.ranking.index(user_3)+1
       end
 
-      it "should confirm the first position to a specific resource in ranking" do
+      it "confirm the first position to a specific resource in ranking" do
         expect(user_3.position).to eq 1
       end
     end
