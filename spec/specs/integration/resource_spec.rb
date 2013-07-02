de158fb5eb8c9b81e350ea2de33c58330334d48f
Joao Moura <joaomdmoura@gmail.com>
2013-07-02 05:02:40 -0300

    updating tests

--- spec/specs/integration/resource_spec.rb
+++ spec/specs/integration/resource_spec.rb
@@ -9,17 +9,11 @@ describe "model updater" do
 
   context "the users play against each other" do
     before :each do
-      50.times {
+      100.times {
         user_1.won(user_1.rating - user_2.rating)
         user_2.lost(user_2.rating - user_1.rating)
-      }
-
-      50.times {
         user_2.won(user_2.rating - user_3.rating)
         user_3.lost(user_3.rating - user_2.rating)
-      }
-
-      50.times {
         user_3.won(user_3.rating - user_4.rating)
         user_4.lost(user_4.rating - user_3.rating)
       }
@@ -30,13 +24,13 @@ describe "model updater" do
     end
 
     it "identify a new user rating with a few matches" do
-      3.times {
-        user_5.won(user_5.rating - user_1.rating)
-        user_1.lost(user_1.rating - user_5.rating)
-      }
       2.times {
         user_5.won(user_5.rating - user_2.rating)
         user_2.lost(user_2.rating - user_5.rating)
+      }
+      2.times {
+        user_5.won(user_5.rating - user_1.rating)
+        user_1.lost(user_1.rating - user_5.rating)
       }
       expect(User.ranking[0]).to eq user_5
     end
